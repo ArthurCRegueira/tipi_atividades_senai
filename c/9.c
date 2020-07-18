d3cacd529255f4f8066d5ec6fc0ec53883aa226b
Arthur Candido Regueira de Miranda <arthur.regueira@aol.com>
2020-07-18 12:30:47 -0300

    Misericórdia

--- c/9.c
+++ c/9.c
@@ -5,27 +5,28 @@
 
 int main(){
     setlocale(LC_ALL,"portuguse_Brasil");
-    double n1, n2, n3, n4, media = 0;
+    float n1, n2, n3, n4, media = 0;
     printf("\t\t Calculador de médias\t\t\n\n");
     printf("Digite nota 1: ");
-    scanf("%f", n1);
+    scanf("%f", &n1);
     printf("\n");
     printf("Digite nota 2: ");
-    scanf("%f", n2);
+    scanf("%f", &n2);
     printf("\n");
     printf("Digite nota 1: ");
-    scanf("%f", n3);
+    scanf("%f", &n3);
     printf("\n");
     printf("Digite nota 1: ");
-    scanf("%f", n4);
+    scanf("%f", &n4);
     printf("\n");
     media = (n1+n2+n3+n4)/4;
+    printf ("A média foi %f", media," portanto");
     if (media >=1){
-        system("color 9");
-        printf("Aluno aprovado!");
+        system("color 09");
+        printf(" o aluno foi aprovado!");
     }else{
-        system(" \ncolor 12");
-        printf(" \nAluno Reprovado!");
+        system("color 12");
+        printf("o aluno foi Reprovado!");
     }
     return 0;
 }
