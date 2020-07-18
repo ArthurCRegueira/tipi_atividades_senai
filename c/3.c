073691c6c3755c2f2ec1c4b59f67547b56a89a1e
Arthur Candido Regueira de Miranda <arthur.regueira@aol.com>
2020-07-17 21:25:51 -0300

    Organização Parcial

---
+++ c/3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    /*
+        1. Toda variável possui um nome
+        2. Toda variável possui um tipo
+        3. Toda variavel possui um tamanho
+        4. Toda variavel posssui um valor
+        a. é temporário
+        b. após o termino o valor da variável é perdido
+        c. todo valor da variavel é volátil
+
+
+        tipo nomeDaVariavel = valor;
+    */
+    int a = 0;
+    int b, c = 0;
+    
+    printf("Digite um número: \n");
+    scan(a);
+    printf("Digite outro número: \n");
+    scanf(b);
+    c = (a+b);
+    printf(%i, c)
+    return 0;
+   /* 
+    int
+    char
+    void
+    double 
+    float
+   */
+}
---
+++ c/4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+    int main(){
+        // printf("tipo de dado", nomeDaVariavel);
+        // printf("%tipo1 %tipo2", var1, var2);
+        int num = 11;
+        printf("%i\n", num);
+        float num2 = 2.4;
+        printf("%f\n", num2);
+        printf("\n\n");
+        /*
+            int %i Ou %d
+            float / double %F
+            char %c
+            str (sting) %s
+
+            %u módulo de um número
+            %p endereço na memória
+            %e ou %E para notação científica
+            %% para imprimir um símblo de porcentagem
+        */
+        int x = 10;
+        float ff = 3.10920;
+        printf("%i - %f\n", x, ff);
+        return 0;
+    }
