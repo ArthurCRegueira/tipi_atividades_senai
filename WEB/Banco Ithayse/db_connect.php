0b714ae3f2aac4980c9e30890692fbd4b7c4bc30
Arthur Candido Regueira de Miranda <arthur.regueira@aol.com>
2020-08-24 16:39:49 -0300

    Temaki

--- WEB/Banco Ithayse/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $serverName = "localhost";
-    $username = "arthur";
-    $password = "password";
-    $db_name = "bi";
-
-    $connect = mysqli_connect($serverName, $username, $password, $db_name);
-?>
