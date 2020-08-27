dab3afb6090b9d55a0e0c7fff3130683e6aee1ef
Arthur Candido Regueira de Miranda <arthur.regueira@aol.com>
2020-08-27 20:32:47 -0300

    Ok!

--- C++/k.cpp
+++ C++/k.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int main(){
-    
+    char resp = 's';
+    int numtab;
+    while (resp != 'n'){
+        cout << "Digite um número para tabuar: ";
+        cin >> numtab;
+        for(int i = 0; i <= 10 ; i++)
+        {
+            cout << numtab << " X " << i <<  " = " << numtab * i << endl;
+        }
+        cout << "Deseja continuar? (s/n): ";
+        cin >> resp;
+    }
 }
