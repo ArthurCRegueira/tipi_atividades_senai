2b6bf9785dfc0eef31a08c84e3030bf4ba0a858c
Arthur Candido Regueira de Miranda <arthur.regueira@aol.com>
2020-09-09 15:07:14 -0300

    Voltei :D

---
+++ Rust/luz/target/debug/deps/luz-bd743ed76fad0f6e.d
@@ -0,0 +1,5 @@
+/home/arthur/Code/Rust/luz/target/debug/deps/luz-bd743ed76fad0f6e: src/main.rs
+
+/home/arthur/Code/Rust/luz/target/debug/deps/luz-bd743ed76fad0f6e.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/luz/target/debug/luz.d
@@ -0,0 +1,1 @@
+/home/arthur/Code/Rust/luz/target/debug/luz: /home/arthur/Code/Rust/luz/src/main.rs
